4e1f6b6c555f95af7a9a56e1bb21b60f646197b3
Nicholas Dyszel <nwd5069@psu.edu>
2012-04-07 16:07:55 -0400

    Added MakeStorage to BoxStorage.h
    yeah

--- boxstorage.cpp
+++ boxstorage.cpp
@@ -3,3 +3,37 @@
 BoxStorage::BoxStorage()
 {
 }
+
+BoxStorage::MakeStorage(istream& cin)
+{
+    int input;
+    int tempStorage[GRID_HEIGHT][GRID_WIDTH];
+    
+    for (int i=0; i<(2*GRID_HEIGHT+1); i++) {
+        for (int j=0; j<(2*GRID_WIDTH+1); j++) {
+            cin >> tempStorage[i][j];
+        }
+    }
+    
+    for (int i=0; i<GRID_HEIGHT; i++) {
+        for (int j=0; j<GRID_WIDTH; j++) {
+            _storage[i][j] = 0b0000;
+            
+            if (tempStorage[2*i+1][2*j] == 1) {
+                _storage[i][j] = _storage[i][j] | SIDE_LEFT;
+            }
+            
+            if (tempStorage[2*i][2*j+1] == 1) {
+                _storage[i][j] = _storag[i][j] | SIDE_UP;
+            }
+         
+            if (tempStorage[2*i+1][2*j+2] == 1) {
+                _storage[i][j] = _storag[i][j] | SIDE_RIGHT;
+            }
+            
+            if (tempStorage[2*i+2][2*j+1] == 1) {
+                _storage[i][j] = _storag[i][j] | SIDE_DOWN;
+            }
+        }
+    }
+}
--- boxstorage.h
+++ boxstorage.h
@@ -1,18 +1,22 @@
 #ifndef BOXSTORAGE_H
 #define BOXSTORAGE_H
 
+#include <istream.h>
+
 #define GRID_HEIGHT 5
 #define GRID_WIDTH  5
 
-#define DIR_LEFT    0b0001
-#define DIR_UP      0b0010
-#define DIR_RIGHT   0b0100
-#define DIR_DOWN    0b1000
+#define SIDE_LEFT    0b0001   // 1 indicates line on that side of box
+#define SIDE_UP      0b0010
+#define SIDE_RIGHT   0b0100
+#define SIDE_DOWN    0b1000
 
 class BoxStorage
 {
 public:
     BoxStorage();
+    
+    MakeStorage(istream& cin);
 
 private:
     short _storage[GRID_HEIGHT][GRID_WIDTH];
