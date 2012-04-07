d9c4abb72d1467269c2d60fd97785c42c6a0f1a3
Nicholas Dyszel <nwd5069@psu.edu>
2012-04-07 17:42:48 -0400

    Added code to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include "boxstorage.h"
+#include "boxgraph.h"
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-
+    BoxStorage board;
+    
+    int playerNumber;
+    cin >> playerNumber;
+    
+    // Populate board
+    board.MakeStorage(cin);
+    
+    // If edge exists that does not give adjacent boxes 3 edges, claim it!
+    Coordinates coordinate = board.BestPosition();
+    if ( (coordinate.X != -1) && (coordinate.Y != -1) ) {
+        cout << coordinate.X << ' ' << coordinate.Y << endl;
+        return 0;
+    }
+    
     return 0;
 }
