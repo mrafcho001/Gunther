61e53912f6302808fa22b27002efe3690add3bdb
Martin Bakiev <mbakiev@gmail.com>
2012-04-07 15:41:41 -0400

    Update

--- boxstorage.h
+++ boxstorage.h
@@ -1,10 +1,22 @@
 #ifndef BOXSTORAGE_H
 #define BOXSTORAGE_H
 
+#define GRID_HEIGHT 5
+#define GRID_WIDTH  5
+
+#define DIR_LEFT    0b0001
+#define DIR_UP      0b0010
+#define DIR_RIGHT   0b0100
+#define DIR_DOWN    0b1000
+
 class BoxStorage
 {
 public:
     BoxStorage();
+
+private:
+    short _storage[GRID_HEIGHT][GRID_WIDTH];
+
 };
 
 #endif // BOXSTORAGE_H
